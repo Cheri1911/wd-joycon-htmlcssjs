89628341eaf2d76d8ac888bc894274aa0bc976f6
Cheri1911 <cherrynovelia@gmail.com>
2025-06-16 20:32:28 +0700

    Menambahkan butto  ab, js, command

---
+++ run.js
@@ -0,0 +1,26 @@
+// HTML button element
+const button = document.querySelector('button');
+
+function up(){
+    document.getElementById("output").innerHTML="Up";
+}
+
+function bottom(){
+    document.getElementById("output").innerHTML="Bottom";
+}
+
+function right(){
+    document.getElementById("output").innerHTML="Right";
+}
+
+function left(){
+    document.getElementById("output").innerHTML="Left";
+}
+
+function a(){
+    document.getElementById("output").innerHTML="A";
+}
+
+function b(){
+    document.getElementById("output").innerHTML="B";
+}
